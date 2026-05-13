56ab116278749f9820526509b1f2b48b43b00251
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2026-05-13 19:19:43 +0300

    Add Apps Script URL for Google Sheets integration

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzr2QYd8Yi3F6A3NikNUwmmmIAgXNBMsPLCMKrol1FPRyG4_x5YRxsRKGD1wwc_aglj/exec'
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyr1hYu_oUgHwD8u9JpD_uhBmrK5hLeq28z5Rd3F3mpjOkIwtmXGcKa9dVKg_ibUKrdwA/exec'
